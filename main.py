98363be80c924e0a0235e530b72219aee914dd4a
Martin Rampanelli <martin.rampanelli@student.uclouvain.be>
2022-03-16 17:43:51 +0100

    truc de base

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # yolo
+
+# il faut vérifier toutes les valeurs
+
+tau = 9 #@valeur taux compression@ #[-]
+D = 0.5 #@valeur alesage@ #[m]
+C = 0.1 #@valeur course@ #[m]
+L = 0.15 #@valeur longueur bielle@ #[m]
+mpiston = 0.5 #@valeur masse piston@ #[kg]
+mbielle = 0.3 #@valeur masse bielle@ #[kg]
+Q = 2 #@valeur chaleur emise par fuel par kg de melange admis@ #[J/kg_inlet gas]
+
+
+
+def myfunc(rpm, s, theta, thetaC, deltaThetaC):
+    #VOTRE CODE
+    return (V_output, Q_output, F_pied_output, F_tete_output, p_output, t)  ;
